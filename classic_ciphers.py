c0ace7a73751777204629262947a56ee384457ca
Jasmine Yan <jasmineyan@college.harvard.edu>
2014-07-08 18:18:50 -0400

    added vignere cipher

--- classic_ciphers.py
+++ classic_ciphers.py
@@ -2,16 +2,20 @@ message = raw_input("To encode: ").replace(' ', '').lower()
 choice = raw_input("What cipher do you want to use? ")
 
 def caesar (message, shift):
-    ans = [chr(ord(c)+shift) for c in message]
+    f = lambda x: chr((ord(x) - 97 + shift) % 26 + 97)
+    ans = [f(c) for c in message]
     return ''.join(ans).upper()
 
 def affine (message, a, b):
-    af = lambda x: (a*x + b)%26
-    ans = [chr( af(ord(c) - 97) + 97) for c in message]
+    f = lambda x: chr((a*(ord(x) - 97) + b) % 26 + 97)
+    ans = [f(c) for c in message]
     return ''.join(ans).upper()
 
 def vignere (message, key):
+    shift = lambda x, y: chr((ord(x) - 97 + y) % 26 + 97)
     k = [ord(c) - 97 for c in key]
+    ans = [shift(message[i], k[i%len(k)]) for i in range(len(message))]
+    return ''.join(ans).upper()
 
 if choice.lower() == "caesar":
     shift = int(raw_input("Shift? (1-25): "))
@@ -21,3 +25,6 @@ elif choice.lower() == "affine":
     a, b = map(int, raw_input("a and b? (1-25): ").split())
     print affine(message, a, b)
 
+elif choice.lower() == "vignere":
+    key = raw_input("Key? ")
+    print vignere(message, key)
